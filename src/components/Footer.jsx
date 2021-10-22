8c634043cd9367d01d922a99f02b19c5bdc1fb29
Osip <arsenij-osipov@mail.ru>
2021-10-22 18:51:37 +0300

    +

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,17 @@ import React from "react";
 function Footer() {
   return (
     <div className="Footer">
-      <p>Development is my passion Задачи:</p>
+      <p>
+        Development is my passion Задачи:
+        <ol>
+          <li>день\ночь</li>
+          <li>Игры </li>
+          <li>Блоки </li>
+          <li>Сертификаты</li>
+          <li>Фотка</li>
+          <li>Fonts?</li>
+        </ol>
+      </p>
       <p>Done with React .JS</p>
     </div>
   );
