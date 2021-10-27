bfe13664c710ad2ce19d83272c7823020ad3f943
Osip <arsenij-osipov@mail.ru>
2021-10-27 11:56:43 +0300

    +

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="Footer">
-      <p>
-        Development is my passion Задачи:
-        <ol>
-          <li>день\ночь</li>
-          <li>Игры </li>
-          <li>Блоки </li>
-          <li>Сертификаты</li>
-          <li>Фотка</li>
-          <li>Fonts?</li>
-        </ol>
-      </p>
+      <p>Development is my passion </p>
       <p>Done with React .JS</p>
     </div>
   );
--- src/components/Iam.jsx
+++ src/components/Iam.jsx
@@ -4,18 +4,14 @@ import Me from "../assets/img/Me.jpg";
 function Iam() {
   return (
     <div className="Iam">
+      <img src={Me} alt="My_photo" />
       <p>
-        <img src={Me} alt="My_photo" />
-        <p className="right">
-          Young,
-          <br /> promising
-          <br /> and confident person.
-        </p>
+        Young, promising and confident person.
         <br />I easily find a common language with people and I like to develop.
-        <br /> I am engaged in programming web applications and I feel that I
-        have found myself in this. I want to become a middle developer in a
-        couple of years, and after a while I want to become a senior. Big plans
-        and big dreams..
+        <br />I am engaged in programming web applications and I feel that I
+        have found myself in this.
+        <br />I want to become a middle developer in a couple of years, and
+        after a while I want to become a senior. Big plans and big dreams..
       </p>
     </div>
   );
